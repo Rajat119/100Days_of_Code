0f972233b01eea54b83059a925acd3ae513e1d01
Rajat Sharma <rajatsharma3327759@gmail.com>
2024-08-10 00:19:59 +0530

    Add files via upload

---
+++ Best Time to Buy and Sell Stock.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0],max=0;
+        for(int i=0;i<prices.length;i++){
+            if(min>prices[i]) min=prices[i];
+             max=Math.max(max,prices[i]-min);
+        }
+        return max;
+    }
+}
