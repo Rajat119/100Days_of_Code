4555f8829a7cef4e97a9ccf7b1e07cc8ce61089d
Rajat Sharma <rajatsharma3327759@gmail.com>
2025-02-12 01:40:14 +0530

    Add files via upload

--- Remove All Occurrences of a Substring.java
+++ Remove All Occurrences of a Substring.java
@@ -1,8 +1,27 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        int Sidx = s.indexOf(part);
-        int Eidx = Sidx+part.length()-1;
-        if(Sidx==-1)return s;
-        return removeOccurrences(s.substring(0,Sidx)+s.substring(Eidx+1),part);
+        char[] input = s.toCharArray();
+        char[] target = part.toCharArray();
+        char[] resultStack = new char[input.length];
+        int stackSize = 0, targetLength = target.length;
+        char targetEndChar = target[targetLength - 1];
+
+        for (char currentChar : input) {
+            resultStack[stackSize++] = currentChar;
+
+            if (currentChar == targetEndChar && stackSize >= targetLength) {
+                int i = stackSize - 1, j = targetLength - 1;
+                
+                while (j >= 0 && resultStack[i] == target[j]) {
+                    i--;
+                    j--;
+                }
+
+                if (j < 0) {
+                    stackSize = i + 1;
+                }
+            }
+        }
+        return new String(resultStack, 0, stackSize);
     }
 }
